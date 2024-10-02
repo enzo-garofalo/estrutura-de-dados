1dd4adb04d618d0918ce98176c023eb6109e7b16
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-10-02 09:20:16 -0300

    Inicio exercício de ponteiro

---
+++ Pointers/duplicates.cpp
@@ -0,0 +1,17 @@
+/*
+Given an integer array nums sorted in non-decreasing order, 
+remove the duplicates in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same. 
+Then return the number of unique elements in nums.
+*/
+#include <iostream>
+using namespace std;
+
+int removeDuplicates(int vec[]){
+
+}
+
+int main(){
+
+    return 0;
+}
