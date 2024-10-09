cbc693208e4e0ee63af983e0e97b37836c23500c
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-10-09 16:34:42 -0300

    Lista encadeada

---
+++ Lista Encadeada/minha_lista.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+};
+
+struct Node* initialize(){
+    return NULL;
+};
+
+/* Perguntar, por que devo fazer retorno? 
+   tem como isso ser void e fds? */
+Node* insertAtBeginning(Node *list, int value){
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = list;
+    return newNode;
+}
+
+void display(Node *list){
+    Node *temp = list;
+    while (temp !=  NULL){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+void search(Node *list, int value){
+    Node *temp = list;
+    bool found = false;
+    while (temp != NULL){
+        if(temp->data == value){
+            found = true;
+            break;
+        }
+        temp = temp->next;
+    }
+    if(found){
+        cout << value <<" Encontrado!!";
+    }else{
+        cout << value <<" Nao Encontrado!!";
+    }
+}
+
+void deleteValue(Node *list, int value){
+    Node *preview = NULL;
+    Node *temp = list;
+
+    if(temp->data == value){
+        list = temp->next;
+        delete temp;
+        return;
+    }
+
+    while(temp != NULL ){
+        if(temp->data == value){
+            preview->next = temp->next;
+            break;
+        }
+        preview = temp;
+        temp = temp->next;
+    }
+
+    if(temp == NULL) cout << "Valor não encontrado! " << endl;
+}
+
+void insertAtPosition(Node *list, int value, int position){
+    // Implementar caso a posição pedida for maior que o limite da lista
+    if(position < 0){
+        cout << "Impossível alocar na posicao " << position;
+        return;
+    }else if(position == 1){
+        list = insertAtBeginning(list, value);
+    }
+    
+    Node *newNode = new Node();
+    newNode->data = value;
+
+    Node *preview = NULL;
+    Node *temp = list;
+
+    int position_counter = 0;
+    while(position_counter != position){
+        preview = temp;
+        temp = temp->next;
+        position_counter++;
+    }
+
+    preview->next = newNode;
+    newNode->next = temp;
+}
+
+int main(){
+    Node *list = initialize();
+
+    int num_elementos;
+    cout << "Digite o número de elementos na lista: ";
+    cin >> num_elementos;
+    
+    //Inserindo Elementos no começo da lista!
+    for(int i = 0; i < num_elementos; i++){
+        int value;
+        cout << "Digite o valor para a posicao ["<< (i) <<"]: ";
+        cin >> value;
+        list = insertAtBeginning(list, value);   
+    }
+       // Percorrendo lista
+    cout << "--------------------------------------------\n";
+    cout << "Lista: ";
+    display(list);
+
+    // Inserindo em uma posição específica
+    int value, position;
+    cout << "--------------------------------------------\n";
+
+    cout << "Digite a posicao para inserir elemento: ";
+    cin >> position;
+    
+    cout << "Digite o elemento: ";
+    cin >> value;
+    insertAtPosition(list, value, position);
+
+    // Percorrendo lista
+    cout << "--------------------------------------------\n";
+    cout << "Lista apos insercao: ";
+    display(list);
+
+    // Realizando buscas
+    cout << "--------------------------------------------\n";
+    cout << "Digite um valor para buscar na lista: ";
+    cin >> value;
+    search(list, value);
+
+    // Removendo ELementos
+    cout << "\n--------------------------------------------\n";
+    cout << "Digite um valor para deletar da lista: ";
+    cin >> value;
+    deleteValue(list, value);
+    cout << "--------------------------------------------\n";
+    cout << "Lista apos remover valor: ";
+    display(list);
+
+    return 0;
+}
