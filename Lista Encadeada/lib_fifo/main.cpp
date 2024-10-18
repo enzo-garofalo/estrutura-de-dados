47c98f2db4ce5c52e0d61d9a5a640eaf92c19038
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-10-18 20:04:56 -0300

    Challenge

--- Lista Encadeada/lib_fifo/main.cpp
+++ Lista Encadeada/lib_fifo/main.cpp
@@ -1,23 +1,95 @@
-#include "lst.h"
 #include <iostream>
-using namespace ENZ_LIB;
+#include <string>
+#include "lib/enzo_lib/qLST.h"
 
-int main() {
-    Queue q;
-    q.first = nullptr;
-    q.last = nullptr;
+using namespace std;
+using namespace enzo_lib;
 
-    Lst lst;
 
-    lst.insert(&q, 10);
-    lst.insert(&q, 20);
-    lst.insert(&q, 30);
+void insertClient(Queue* fila)
+{
+    Client* newClient = new Client; 
+    cout << "\n=================================\n";
+    cout << "Digite o ID do Cliente: ";
+    cin >> newClient->ID;
+    cin.ignore(); 
+    cout << "Digite o nome do Cliente: ";
+    getline(cin, newClient->name);
 
-    lst.display(&q);
+    qLST lst;
+    lst.insert(fila, newClient);
+    cout << "=================================\n";
+    cout << "    Novo Cliente na Fila ";
+    cout << "\n=================================\n";
 
-    lst.remove(&q);
+}
 
-    lst.display(&q);
+void attendClient(Queue* fila)
+{
+    if(fila->first != NULL)
+    {
+        cout << "\n=================================\n";
+        cout << "Atendendo: " << fila->first->client.name;
+        cout << "\nID: " << fila->first->client.ID;
+        cout << "\n=================================\n";
+    
+        qLST lst;
+        lst.remove(fila);
+    }else{
+        cout << "\n=================================\n";
+        cout << "       Nao ha clientes na fila";
+        cout << "\n=================================\n";
+    }
+    
+}
+
+int main()
+{
+    qLST lst;
+    Queue *fila = new Queue;
+    fila->first = NULL;
+    fila->last = NULL;
+    int choice;
+
+    do 
+    {
+        cout << "Menu:\n";
+        cout << "1. Adicionar cliente a fila\n";
+        cout << "2. Atender cliente\n";
+        cout << "3. Exibir clientes na fila\n";
+        cout << "4. Liberar memoria da fila\n";
+        cout << "0. Sair\n";
+        cout << "Escolha uma opcao: ";
+        cin >> choice;
+
+        switch (choice) 
+        {
+            case 1: 
+                insertClient(fila);
+                break;
+            case 2:
+                attendClient(fila);
+                break;
+            case 3:
+                if(fila->first == NULL){
+                    cout << "\n=================================\n";
+                    cout << "       Nao ha clientes na fila";
+                    cout << "\n=================================\n";
+                }else{
+                    lst.display(fila);
+                }
+                break;
+            case 4:
+                break;
+            case 0:
+                break;
+            default:
+                cout << "Opção inválida. Tente novamente." << endl;
+        }
+        cout << endl; 
+    } while (choice != 0);
+
+
 
     return 0;
 }
